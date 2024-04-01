9782843dab5438333783b263c6ff3c743fe892d8
MarianoJSB <mariano.n.aguiar@gmail.com>
2024-04-01 13:52:44 -0300

    nv

--- src/proyects/Memory-game/Juego/Comparar.js
+++
@@ -1,8 +0,0 @@
-function comparar(tarjetasComparadas){//Funcion para comparar si las tarjetas seleccionadas son iguales
-    if (tarjetasComparadas[0].dataset.valor === tarjetasComparadas[1].dataset.valor){
-        adivinarBien(tarjetasComparadas)//Llamamos a funcion en caso de adivinar las cartas 
-    }
-    else{
-        adivinarMal(tarjetasComparadas)//SetTimeOut es una funcion para establacer en tiempo determinado a una funcion
-    }
-}
